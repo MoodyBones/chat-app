832be4ef572efcfa0aa42ddb2bc2fac386fd81c4
Mel <jones.mel@gmail.com>
2019-07-09 20:06:38 +0200

    event listeners onSubmit | onChange

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
 class SendMessageForm extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            message: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(e) {
+        // to test
+        // console.log(e.target.value)
+        this.setState({
+            message: e.target.value
+        })
+
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        console.log(this.state.message)
+        // send off the message
+    }
+
     render() {
         return (
-            <form className="send-message-form">
+            <form 
+                onSubmit={this.handleSubmit}
+                className="send-message-form">
                 <input
-                    placeholder="SendMessageForm"
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Type your message and hit ENTER"
                     type="text" />
             </form>
         )
